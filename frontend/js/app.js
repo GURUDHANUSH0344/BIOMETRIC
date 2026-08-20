39157b024b53d80e6de903023c7be04c1cdd1a86
gowtham072007 <gowthamaa.ug.25.ad@francisxavier.ac.in>
2026-08-20 14:33:54 +0530

    Add universal XSS escaping, cache-busting v2.1.0 to all frontend pages, and null-safe DOM bindings

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -125,3 +125,21 @@ const App = {
 };
 
 window.App = App;
+
+window.escapeHtml = function(str) {
+  if (str === null || str === undefined) return '';
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+};
+
+window.escapeAttr = function(str) {
+  if (str === null || str === undefined) return '';
+  return String(str)
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+};
+
